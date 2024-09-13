f8af07d3da4446eedea316abf13868498dc1219e
e.davydov <davidoveg@gmail.com>
2024-09-13 21:42:06 +0700

    не могу открыть файл в java

--- src/main/java/ru/improve/Main.java
+++ src/main/java/ru/improve/Main.java
@@ -3,17 +3,41 @@ package ru.improve;
 import ru.improve.dz1.equationSolve.KubSolve;
 import ru.improve.dz1.functions.CubFunction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        try (InputStream inputStream = Main.class.getResourceAsStream("dav.txt");
+             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
 
-        CubFunction cubFunction = new CubFunction(1, 2, -1, -2);
+            KubSolve kubSolve = new KubSolve();
 
-        KubSolve kubSolve = new KubSolve();
-        List<Double> solveList = kubSolve.solve(cubFunction, 0.1, 0.2);
-        for (double solve : solveList) {
-            System.out.print(solve + " ");
+            String line;
+            for (int i = 0; i < 25; i++) {
+                line = br.readLine();
+
+                List<Double> doubleList = Arrays.stream(line.split(" "))
+                        .map(str -> Double.parseDouble(str))
+                        .collect(Collectors.toList());
+
+                CubFunction cubFunction = new CubFunction(1, doubleList.get(0), doubleList.get(1), doubleList.get(2));
+                List<Double> solveList = kubSolve.solve(cubFunction, doubleList.get(3), doubleList.get(4));
+
+                System.out.print(solveList.size());
+                for (double solve : solveList) {
+                    System.out.print(" " + solve);
+                }
+                System.out.println();
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
