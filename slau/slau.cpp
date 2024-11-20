616403693f4fb7908d877ce492a95f41a9e53e62
e.davydov <e.davydov@g.nsu.ru>
2024-11-21 00:05:12 +0700

    commit

--- slau/slau.cpp
+++ slau/slau.cpp
@@ -3,28 +3,39 @@
 
 using namespace std;
 
+class ModuleFunc {
+
+public:
+    double value(double x) {
+        return abs(x);
+    }
+};
+
 class Spline {
 
 private:
-    double ai = 0;
-    double bi = 0;
-    double ci = 0;
-    double di = 0;
+    double ai;
+    double bi;
+    double ci;
+    double di;
 
-    double xi = 0;
-    double xi1 = 0;
-
+    double hi;
 public:
+
+    void setHi(double hi) {
+        this->hi = hi;
+    }
+
     double spline() {
-        return ai + bi * hi() + ci / 2.0 * pow(hi(), 2) + di / 6.0 * pow(hi(), 3);
+        return ai + bi * hi + ci / 2.0 * pow(hi, 2) + di / 6.0 * pow(hi, 3);
     }
 
     double der1() {
-        return bi + ci * hi() + di / 2.0 * pow(hi(), 2);
+        return bi + ci * hi + di / 2.0 * pow(hi, 2);
     }
 
     double der2() {
-        return ci + di * hi();
+        return ci + di * hi;
     }
 
     double setNextSplineRatio(double ai, double bi, double ci, double di) {
@@ -34,19 +45,23 @@ public:
         this->di = di;
     }
 
-    double setNextXi(double xi1) {
-        this->xi = this->xi1;
-        this->xi1 = xi1;
-    }
+    void calcAndSetNextAi() {
 
-private:
-    double hi() {
-        return xi1 - xi;
     }
 };
 
-int main() {
-    cout << "hello world" << endl;
+double createSpline(Spline &spline, ModuleFunc &moduleFunc) {
 
+}
+
+int main() {
+    Spline spline;
+    ModuleFunc moduleFunc;
+
+    double a = -1;
+    double b = 1;
+    int pointsNumber = 5;
+
+    double hi = (abs(a) + abs(b)) / abs(pointsNumber);
     return 0;
 }
