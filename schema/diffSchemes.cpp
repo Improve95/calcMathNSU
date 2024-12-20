89207a75bf7a297933cf5c0d0f8328d916592618
e.davydov <e.davydov@g.nsu.ru>
2024-12-20 23:40:16 +0700

    разностные схемы initial

---
+++ schema/diffSchemes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a = 0, b = 10;
+    int n1 = 100, n2 = 1000;
+    double h1 = (b - a) / n1, h2 = (b - a) / n2;
+
+    double t11 = 0.25 * h1, t12 = 0.5 * h1, t13 = 1 * h1, t14 = 1.25 * h1;
+    double t21 = 0.25 * h1, t22 = 0.5 * h1, t23 = 1 * h1, t24 = 1.25 * h1;
+
+    int T1 = 1, T2 = 2;
+
+    
+
+    return 0;
+}
