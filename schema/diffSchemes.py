cb798edae61aefc87ddc315aaa09b4ccb60602e4
e.davydov <e.davydov@g.nsu.ru>
2024-12-25 22:34:24 +0700

    русанов завелся

--- schema/diffSchemes.py
+++ schema/diffSchemes.py
@@ -39,34 +39,69 @@ def godunovTask1(j, n, tao, h, N, coords):
 def godunovTask2(j, n, tao, h, N, coords):
     return (-1) * (tao / h) * (pow(coords[j][n], 2) / 2 - pow(coords[(j - 1) % N][n], 2) / 2) + coords[j][n]
 
+def calcRusanovTask1(tao, h, N, k):
+    k = int(k)
+    cs = np.zeros((N + 1, k))
 
-def calcWj_t1(j, n, N, cs):
-    return cs[(j + 2) % N][n] - 4 * cs[(j + 1) % N][n] + 6 * cs[j % N][n] - 4 * cs[(j - 1) % N][n] + cs[(j - 2) % N][n]
-
-def calcF12_t1(j, n, r, N, cs):
-    return (cs[j % N][n] + cs[(j + 1) % N][n]) / 2 - r / 3 * (cs[(j + 1) % N][n] - cs[j % N][n])
-
-def calcF2_t1(j, n, r, N, cs):
-    return cs[j % N][n] - 2 * r / 3 * (calcF12_t1(j, n, r, N, cs) - calcF12_t1(j - 1, n, r, N, cs))
-
-def rusanovTask1(j, n, tao, h, N, cs):
     r = tao / h
-    return cs[j][n] - r / 24.0 * (7 * (cs[(j + 1) % N][n] - cs[(j - 1) % N][n]) - 2 * (cs[(j + 2) % N][n] - cs[(j - 2) % N][n])) \
-            - 3 / 8 * r * (calcF12_t1(j + 1, n, r, N, cs) - calcF12_t1(j - 1, n, r, N, cs)) - 0.104 * calcWj_t1(j, n, N, cs)
 
-def calcWj_t2(j, n, N, cs):
-    return cs[(j + 2) % N][n] - 4 * cs[(j + 1) % N][n] + 6 * cs[j % N][n] - 4 * cs[(j - 1) % N][n] + cs[(j - 2) % N][n]
+    xcs, ycs = [], []
+    f12, f2, w = np.zeros((N + 1, k)), np.zeros((N + 1, k)), np.zeros((N + 1, k))
 
-def calcF12_t2(j, n, r, N, cs):
-    return (cs[j % N][n] + cs[(j + 1) % N][n]) / 2 - r / 3 * (pow(cs[(j + 1) % N][n], 2) / 2 - pow(cs[j % N][n], 2) / 2)
+    for j in range (N + 1):
+        cs[j][0] = Sinus.value(j * h)
+    
+    for n in range (k - 1):
+        for j in range (N + 1): 
+            f12[j][n] = (cs[j][n] + cs[(j + 1) % N][n]) / 2 - r / 3 * (cs[(j + 1) % N][n] - cs[j][n])
+        
+        for j in range (N + 1): 
+            f2[j][n] = cs[j][n] - 2 * r / 3 * (f12[j][n] - f12[(j - 1) % N][n])
+        
+        for j in range (N + 1): 
+            w[j][n] = cs[(j + 2) % N][n] - 4 * cs[(j + 1) % N][n] + 6 * cs[j][n] - 4 * cs[(j - 1) % N][n] + cs[(j - 2) % N][n]
+        
+        for j in range (N + 1): 
+            cs[j][n + 1] = cs[j][n] - r / 24 * (7 * (cs[(j + 1) % N][n] - cs[(j - 1) % N][n]) - 2 * (cs[(j + 2) % N][n] - cs[(j - 2) % N][n])) \
+            - 3 * r / 8 * (f2[(j + 1) % N][n] - f2[(j - 1) % N][n]) - 0.104 * w[j][n]
 
-def calcF2_t2(j, n, r, N, cs):
-    return cs[j % N][n] - 2 * r / 3 * (pow(calcF12_t2(j, n, r, N, cs), 2) / 2 - pow(calcF12_t2(j - 1, n, r, N, cs), 2) / 2)
+    for j in range(N):
+        xcs.append(j * h)
+        ycs.append(cs[j][k - 1])
 
-def rusanovTask2(j, n, tao, h, N, cs):
+    return xcs, ycs
+
+def calcRusanovTask2(tao, h, N, k):
+    k = int(k)
+    cs = np.zeros((N + 1, k))
+
     r = tao / h
-    return cs[j][n] - r / 24.0 * (7 * (pow(cs[(j + 1) % N][n], 2) / 2 - pow(cs[(j - 1) % N][n], 2) / 2) - 2 * (pow(cs[(j + 2) % N][n], 2) / 2 - pow(cs[(j - 2) % N][n], 2) / 2)) \
-            - 3 / 8 * r * (pow(calcF12_t2(j + 1, n, r, N, cs), 2) / 2 - pow(calcF12_t2(j - 1, n, r, N, cs), 2) / 2) - 0.104 * calcWj_t2(j, n, N, cs)
+
+    xcs, ycs = [], []
+    f12, f2, w = np.zeros((N + 1, k)), np.zeros((N + 1, k)), np.zeros((N + 1, k))
+
+    for j in range (N + 1):
+        cs[j][0] = Sinus.value(j * h)
+    
+    for n in range (k - 1):
+        for j in range (N + 1): 
+            f12[j][n] = (cs[j][n] + cs[(j + 1) % N][n]) / 2 - r / 3 * (pow(cs[(j + 1) % N][n], 2) / 2 - pow(cs[j][n], 2) / 2)
+        
+        for j in range (N + 1): 
+            f2[j][n] = cs[j][n] - 2 * r / 3 * (pow(f12[j][n], 2) / 2 - pow(f12[(j - 1) % N][n], 2) / 2)
+        
+        for j in range (N + 1): 
+            w[j][n] = cs[(j + 2) % N][n] - 4 * cs[(j + 1) % N][n] + 6 * cs[j][n] - 4 * cs[(j - 1) % N][n] + cs[(j - 2) % N][n]
+        
+        for j in range (N + 1): 
+            cs[j][n + 1] = cs[j][n] - r / 24 * (7 * (pow(cs[(j + 1) % N][n], 2) / 2 - pow(cs[(j - 1) % N][n], 2) / 2) - 2 * (pow(cs[(j + 2) % N][n], 2) / 2 - pow(cs[(j - 2) % N][n], 2) / 2)) \
+            - 3 * r / 8 * (pow(f2[(j + 1) % N][n], 2) / 2 - pow(f2[(j - 1) % N][n], 2) / 2) - 0.104 * w[j][n]
+
+    for j in range(N):
+        xcs.append(j * h)
+        ycs.append(cs[j][k - 1])
+
+    return xcs, ycs
 
 def calculateGraphs(tao, h, N, k, method):
     k = int(k)
@@ -97,10 +132,31 @@ def calculateGraphsWithAnyParameters(a, b, N12, T12, info: str, task, method):
     for N in N12:
         for T in T12:
             h = (b - a) / float(N)
-            multiplier = 0.0
+            multiplier = 0.25
 
-            for kt in range(4):
+            for multiplier in [0.25, 0.5, 1.0, 1.25]:
+                if (task == 1):
+                    tao = h * multiplier
+                else:
+                    tao = h * multiplier / 4
+                k = T / tao
+
+                graphData = f"info: N={N}, T={T}, h=h, tao={round(tao, 6)}, k={round(k, 2)}"
+                xcs, ycs = calculateGraphs(tao, h, N, k, method)
+                graph = Graph(graphData, xcs, ycs)
+                graphs.append(graph)
                 multiplier += 0.25
+    
+    return graphs
+
+def calculateRusanovGraphsWithAnyParameters(a, b, N12, T12, info: str, task, rusanovTaskFunc):
+    graphs = []
+    for N in N12:
+        for T in T12:
+            h = (b - a) / float(N)
+            multiplier = 0.25
+
+            for multiplier in [0.25, 0.5, 1.0, 1.25]:
                 if (task == 1):
                     tao = h * multiplier
                 else:
@@ -108,9 +164,10 @@ def calculateGraphsWithAnyParameters(a, b, N12, T12, info: str, task, method):
                 k = T / tao
 
                 graphData = f"{info}: N={N}, T={T}, h={h}, tao={round(tao, 6)}, k={round(k, 2)}"
-                xcs, ycs = calculateGraphs(tao, h, N, k, method)
+                xcs, ycs = rusanovTaskFunc(tao, h, N, k)
                 graph = Graph(graphData, xcs, ycs)
                 graphs.append(graph)
+                multiplier += 0.25
     
     return graphs
 
@@ -121,14 +178,14 @@ def main():
 
     totalGraphs = []
 
-    totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, T12, "forward-t1", 1, forwardTask1)
-    totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, [0.22, 0.25], "forward-t2", 2, forwardTask2)
+    # totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, T12, "forward-t1", 1, forwardTask1)
+    # totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, [0.22, 0.25], "forward-t2", 2, forwardTask2)
 
-    totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, T12, "godunov-t1", 1, godunovTask1)
-    totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, T12, "godunov-t2", 2, godunovTask2)
+    # totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, T12, "godunov-t1", 1, godunovTask1)
+    # totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, T12, "godunov-t2", 2, godunovTask2)
 
-    totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, T12, "rusanov-t1", 1, rusanovTask1)
-    totalGraphs += calculateGraphsWithAnyParameters(a, b, N12, T12, "rusanov-t2", 2, rusanovTask2)
+    totalGraphs += calculateRusanovGraphsWithAnyParameters(a, b, N12, T12, "rusanov-t1", 1, calcRusanovTask1)
+    totalGraphs += calculateRusanovGraphsWithAnyParameters(a, b, N12, T12, "rusanov-t2", 2, calcRusanovTask2)
 
     drawGraphs(totalGraphs)
 
